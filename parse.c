8e27a994c3421692b1ed97bc63625cb0671967f9
Sumshi <sumayasomow@gmail.com>
2023-05-24 11:51:12 +0000

    handles path

--- parse.c
+++ parse.c
@@ -7,54 +7,74 @@
  */
 void parseInput(char *input, char **args)
 {
+	int last_exit_status = 0;
 	char *token;
 	int i = 0;
+	int j;
+	char pid[10];
+	char exit_status[10];
 
 	token = _strtok(input, " ");
 	while (token != NULL)
 	{
-		char *output = handleVariables(token);
-
-		args[i++] = output;
-
+		if (token[0] == '#')
+		{
+			break;
+		}
+		args[i++] = token;
 		token = _strtok(NULL, " ");
 	}
 	args[i] = NULL;
+	/* Replace $?, $$ and $PATH variables */
+	for (j = 0; args[j] != NULL; j++)
+	{
+		if (_strcmpr(args[j], "$?") == 0)
+		{
+			int len = int_to_string(exit_status, last_exit_status);
+
+			write_string(args[j], exit_status, len);
+		}
+		else if (_strcmpr(args[j], "$$") == 0)
+		{
+			int len = int_to_string(pid, getpid());
+
+			write_string(args[j], pid, len);
+		}
+		else if (_strcmpr(args[j], "$PATH") == 0)
+		{
+			args[j] = _strdup(getenv("PATH"));
+		}
+	}
 }
+
 /**
- * handleVariables - handles $, $$
- * @input: input passed.
- * Return: Always 0.
+ * int_to_string - converts an integer to a string
+ * @str: destination buffer to store the string
+ * @num: the integer to convert
+ * Return: The length of the string
  */
-char *handleVariables(char *input)
+int int_to_string(char *str, int num)
 {
-	char *output = NULL;
-	int last_exit_status = 0;
+	int i = 0;
+	int is_negative = 0;
 
-	if (_strcmpr(input, "$") == 0)
+	/*Handle negative numbers*/
+	if (num < 0)
 	{
-		output = getenv("HOME");
+		is_negative = 1;
+		num = -num;
 	}
-	else if (_strcmpr(input, "$$") == 0)
+	/*Convert each digit to a character in reverse order*/
+	do {
+		str[i++] = num % 10 + '0';
+		num /= 10;
+	} while (num > 0);
+	/*Add the negative sign if necessary*/
+	if (is_negative)
 	{
-		int pid = getpid();
-
-		output = malloc(sizeof(char) * 16);
-		sprintf(output, "%d", pid);
+		str[i++] = '-';
 	}
-	else if (_strcmpr(input, "$PATH") == 0)
-	{
-		output = getenv("PATH");
-	}
-	else if (_strcmpr(input, "$?") == 0)
-	{
-
-		output = malloc(sizeof(char) * 16);
-		sprintf(output, "%d", last_exit_status);
-	}
-	else
-	{
-		output = _strdup(input);
-	}
-	return (output);
+	/*Reverse the string*/
+	reverse_string(str, i);
+	return (i);
 }
