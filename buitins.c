c1b66e57e758e5d3dda9347135e33be9a7b261e1
Sumshi <sumayasomow@gmail.com>
2023-05-24 08:08:14 +0000

    exit

--- buitins.c
+++ buitins.c
@@ -9,9 +9,18 @@ void check_exit(char **args)
 	int stats;
 
 	stats = EXIT_SUCCESS;
-	if (args[1] != NULL)
+	if (args[1] != NULL && args[1][0] != '\0')
 	{
-		stats = _atoi(args[1]);
+		char *endptr;
+		long val = strtol(args[1], &endptr, 10);
+
+		if (*endptr != '\0' || val > INT_MAX || val < INT_MIN)
+		{
+			fprintf(stderr, "Invalid argument: %s\n", args[1]);
+			return;
+		}
+
+		stats = (int)val;
 	}
 	exit(stats);
 }
