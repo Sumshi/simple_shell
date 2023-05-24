eb5a18f06ad758b23307ecf1f6770f1bcad9913f
Sumshi <sumayasomow@gmail.com>
2023-05-24 09:08:17 +0000

    exit

--- exit.c
+++ exit.c
@@ -1,9 +1,14 @@
 #include "main.h"
 /**
  * exitShell - exits shell
+ * @exitStatus: value
  * Return: always 0
  **/
 void exitShell(int exitStatus)
 {
+	if (exitStatus < 0 || exitStatus > 255)
+	{
+		perror("invalid exit status");
+	}
 	exit(exitStatus);
 }
