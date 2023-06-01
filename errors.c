910d1a9d3069c7dc75f6b86219ef0c0e3db6bf60
Sumshi <sumayasomow@gmail.com>
2023-06-01 08:08:46 +0000

    fixed

--- errors.c
+++ errors.c
@@ -1,46 +1,111 @@
-#include "main.h"
+#include "root.h"
 
 /**
- * ctrl_c - detect the CTRL+C signal and print another line with the prompt.
- * @x: idk.
+ * num_len - Counts the digit length of a number.
+ * @num: The number to measure.
+ *
+ * Return: The digit length.
  */
-void ctrl_c(__attribute__((unused)) int x)
+int num_len(int num)
 {
-	signal(SIGINT, ctrl_c);
-	write(1, "\n", 1);
-	write(STDOUT_FILENO, "\033[0;36mhsh# \033[0m", 16);
+	unsigned int num1;
+	int len = 1;
+
+	if (num < 0)
+	{
+		len++;
+		num1 = num * -1;
+	}
+	else
+	{
+		num1 = num;
+	}
+	while (num1 > 9)
+	{
+		len++;
+		num1 /= 10;
+	}
+
+	return (len);
 }
 
 /**
- * print_error - print specific errors to standard output.
- * @program_name: argv[0] of main.
- * @input: command that produces the error.
- * @error_num: error number - identifies the error type.
+ * _itoa - Converts an integer to a string.
+ * @num: The integer.
+ *
+ * Return: The converted string.
  */
-void print_error(char *program_name, char *input, int error_num)
+char *_itoa(int num)
 {
-	char *str;
+	char *buffer;
+	int len = num_len(num);
+	unsigned int num1;
 
-	if (error_num == 127) /* command not found */
+	buffer = malloc(sizeof(char) * (len + 1));
+	if (!buffer)
+		return (NULL);
+
+	buffer[len] = '\0';
+
+	if (num < 0)
 	{
-		write(STDOUT_FILENO, program_name, _strlen(program_name));
-		write(STDOUT_FILENO, ": 1: ", 5);
-		write(STDOUT_FILENO, input, _strlen(input));
-		write(STDOUT_FILENO, ": not found\n", 12);
+		num1 = num * -1;
+		buffer[0] = '-';
 	}
-	if (error_num == 2) /* syntax error*/
+	else
 	{
-		str = "sh: 1: Syntax error: \"";
-		write(STDOUT_FILENO, str, _strlen(str));
-		write(STDOUT_FILENO, ";", 1);
-		str = "\" unexpected\n";
-		write(STDOUT_FILENO, str, _strlen(str));
+		num1 = num;
 	}
-	if (error_num == 3) /* malloc can't allocate memory */
+
+	len--;
+	do {
+		buffer[len] = (num1 % 10) + '0';
+		num1 /= 10;
+		len--;
+	} while (num1 > 0);
+
+	return (buffer);
+}
+
+
+/**
+ * create_error - Writes a custom error message to stderr.
+ * @args: An array of arguments.
+ * @err: The error value.
+ *
+ * Return: The error value.
+ */
+int create_error(char **args, int err)
+{
+	char *error;
+
+	switch (err)
 	{
-		write(STDOUT_FILENO, program_name, _strlen(program_name));
-		write(STDOUT_FILENO, ": 1: ", 5);
-		str = "internal error allocating memory\n";
-		write(STDOUT_FILENO, str, _strlen(str));
+	case -1:
+		error = error_env(args);
+		break;
+	case 1:
+		error = error_1(args);
+		break;
+	case 2:
+		if (*(args[0]) == 'e')
+			error = error_2_exit(++args);
+		else if (args[0][0] == ';' || args[0][0] == '&' || args[0][0] == '|')
+			error = error_2_syntax(args);
+		else
+			error = error_2_cd(args);
+		break;
+	case 126:
+		error = error_126(args);
+		break;
+	case 127:
+		error = error_127(args);
+		break;
 	}
+	write(STDERR_FILENO, error, _strlen(error));
+
+	if (error)
+		free(error);
+	return (err);
+
 }
--- getline.c
+++ getline.c
@@ -1,49 +1,135 @@
-#include "main.h"
-/**
- * my_getline - getline implementation
- * @data: data to be read
- * Return: characters read
- */
-ssize_t my_getline(main_t *data)
-{
-	char *cursor_ptr, *end_ptr, c;
-	size_t size = BUFFER_SIZE, read_line, length, new_size;
+#include "root.h"
 
-	data->line = malloc(size * sizeof(char));
-	if (data->line == NULL)
+/**
+ * _realloc - Reallocates a memory block using malloc and free.
+ * @ptr: A pointer to the memory previously allocated.
+ * @old_size: The size in bytes of the allocated space for ptr.
+ * @new_size: The size in bytes for the new memory block.
+ *
+ * Return: If new_size == old_size - ptr.
+ *         If new_size == 0 and ptr is not NULL - NULL.
+ *         Otherwise - a pointer to the reallocated memory block.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *mem;
+	char *ptr_copy, *filler;
+	unsigned int index;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
 	{
-		return (FAIL);
+		mem = malloc(new_size);
+		if (mem == NULL)
+			return (NULL);
+
+		return (mem);
 	}
-	if (isatty(STDIN_FILENO))
+
+	if (new_size == 0 && ptr != NULL)
 	{
-		PRINT(PROMPT);
+		free(ptr);
+		return (NULL);
 	}
-	for (cursor_ptr = data->line, end_ptr = data->line + size;;)
+
+	ptr_copy = ptr;
+	mem = malloc(sizeof(*ptr_copy) * new_size);
+	if (mem == NULL)
 	{
-		read_line = read(STDIN_FILENO, &c, 1);
-		if (read_line == 0)
-		{
-			return (FAIL);
-		}
-		*cursor_ptr++ = c;
-		if (c == '\n')
-		{
-			*cursor_ptr = '\0';
-			return (SUCCESS);
-		}
-		if (cursor_ptr + 2 >= end_ptr)
-		{
-			new_size = size * 2;
-			length = cursor_ptr - data->line;
-			data->line = my_realloc(data->line, size * sizeof(char),
-					new_size * sizeof(char));
-			if (data->line == NULL)
-			{
-				return (FAIL);
-			}
-			size = new_size;
-			end_ptr = data->line + size;
-			cursor_ptr = data->line + length;
-		}
+		free(ptr);
+		return (NULL);
 	}
+
+	filler = mem;
+
+	for (index = 0; index < old_size && index < new_size; index++)
+		filler[index] = *ptr_copy++;
+
+	free(ptr);
+	return (mem);
+}
+
+/**
+ * assign_lineptr - Reassigns the lineptr variable for _getline.
+ * @lineptr: A buffer to store an input string.
+ * @n: The size of lineptr.
+ * @buffer: The string to assign to lineptr.
+ * @b: The size of buffer.
+ */
+void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
+{
+	if (*lineptr == NULL)
+	{
+		if (b > 120)
+			*n = b;
+		else
+			*n = 120;
+		*lineptr = buffer;
+	}
+	else if (*n < b)
+	{
+		if (b > 120)
+			*n = b;
+		else
+			*n = 120;
+		*lineptr = buffer;
+	}
+	else
+	{
+		_strcpy(*lineptr, buffer);
+		free(buffer);
+	}
+}
+
+/**
+ * _getline - Reads input from a stream.
+ * @lineptr: A buffer to store the input.
+ * @n: The size of lineptr.
+ * @stream: The stream to read from.
+ *
+ * Return: The number of bytes read.
+ */
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
+{
+	static ssize_t input;
+	ssize_t ret;
+	char c = 'x', *buffer;
+	int r;
+
+	if (input == 0)
+		fflush(stream);
+	else
+		return (-1);
+	input = 0;
+
+	buffer = malloc(sizeof(char) * 120);
+	if (!buffer)
+		return (-1);
+
+	while (c != '\n')
+	{	r = read(STDIN_FILENO, &c, 1);
+		if (r == -1 || (r == 0 && input == 0))
+		{
+			free(buffer);
+			return (-1);
+		}
+		if (r == 0 && input != 0)
+		{
+			input++;
+			break;
+		}
+
+		if (input >= 120)
+			buffer = _realloc(buffer, input, input + 1);
+
+		buffer[input] = c;
+		input++;
+	}	buffer[input] = '\0';
+	assign_lineptr(lineptr, n, buffer, input);
+	ret = input;
+	if (r != 0)
+		input = 0;
+	return (ret);
 }
